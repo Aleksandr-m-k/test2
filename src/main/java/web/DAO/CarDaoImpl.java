fe304086805c305bddab87a31ee977e51ebb6c8a
Aleksandr-m-k <a.k.85@mail.ru>
2024-05-31 01:16:12 +0200

    Решение №2

---
+++ src/main/java/web/DAO/CarDaoImpl.java
@@ -0,0 +1,28 @@
+package web.DAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CarDaoImpl implements CarDao {
+
+    public List<Car> getCarsByNumber(int count) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("BMV", 5, 2012));
+        cars.add(new Car("WV", 7, 2014));
+        cars.add(new Car("Lada", 2110, 2006));
+        cars.add(new Car("mercedes-benz", 123, 2007));
+        cars.add(new Car("Kia", 5, 2017));
+        if (count > 5) {
+            return cars;
+        } else {
+            return cars.stream().limit(count).collect(Collectors.toList());
+        }
+    }
+}
+
